1d289aa4e550102f4ecfb485017930667b184edb
mot25 <76829487+mot25@users.noreply.github.com>
2022-02-28 12:15:30 +0200

    color and fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,20 @@ import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <>
-  <Head>
-    <title>My App Top on Next js</title>
-  </Head>
-  <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <title>My App Top on Next js</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
-  
