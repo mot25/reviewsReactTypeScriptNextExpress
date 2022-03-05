3723f99a594aad641afbe0b1f6f34d663367a13a
mot25 <76829487+mot25@users.noreply.github.com>
2022-03-06 00:23:32 +0200

    ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,65 @@
+import { GetStaticProps } from "next";
 import React, { useState } from "react";
-import Button from "../components/button/Button";
-import Htag from "../components/Htag/Htag";
-import Paragraf from "../components/paragraf/Paragraf";
-import Rating from "../components/Rating/Rating";
-import Tag from "../components/tag/Tag";
 import { withLayout } from "../layout/Layout";
+import axios from "axios";
+import Htag from "../components/Htag/Htag";
+import Button from "../components/button/Button";
+import Tag from "../components/tag/Tag";
+import Rating from "../components/Rating/Rating";
+import { MenuItem } from "../interfaces/menu.interfaces";
 
-function Home(): JSX.Element {
-  const [rat, setRating] = useState<number>(4);
+function Home({ menu }: HomeProps): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
 
   return (
     <>
-      <Htag tag="h1">titel</Htag>
-      <Button mode="primary">tesdthd</Button>
-      <Button
-        arrow="right"
-        mode="ghost"
-        onClick={() => {
-          console.log("wergawegere");
-        }}
-      >
-        tesdthd
+      <Htag tag="h1">Заголовок</Htag>
+      <Button mode="primary" arrow="right">
+        Кнопка
       </Button>
-      <Tag color="ghost">ghost</Tag>
-      <Tag color="red">red</Tag>
-      <Tag color="grey">grey</Tag>
-      <Tag color="green">green</Tag>
-      <Tag href="#" color="primary">
-        primary
+      <Button mode="ghost" arrow="down">
+        Кнопка
+      </Button>
+
+      <Tag size="s">Ghost</Tag>
+      <Tag size="r" color="red">
+        Red
       </Tag>
-      <Rating rating={rat} isEditable={true} setRating={setRating} />
+      <Tag size="s" color="green">
+        Green
+      </Tag>
+      <Tag color="primary">Green</Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu.map((item, i) => {
+          return <li key={i}>{item._id.secondCategory}</li>;
+        })}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    "https://courses-top.ru/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
+  console.log(menu);
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
